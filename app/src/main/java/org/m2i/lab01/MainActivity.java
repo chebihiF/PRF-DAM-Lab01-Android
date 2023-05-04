23afb1ebc531ac3a1f3fe0af3e3216e751afd4b7
Fayçal Chebihi <chebihifaycal.it@gmail.com>
2023-05-04 15:52:04 +0100

    version 1.0

--- app/src/main/java/org/m2i/lab01/MainActivity.java
+++ app/src/main/java/org/m2i/lab01/MainActivity.java
@@ -3,12 +3,70 @@ package org.m2i.lab01;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
+
+    Random random = new Random();
+    int random_nbr, count=0; // 5 ?
+    EditText nbr_txt;
+    TextView response_txt;
+    Button submit_btn, new_btn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        // generate random number
+        random_nbr = random.nextInt(100);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        nbr_txt = findViewById(R.id.nbr_txt);
+        response_txt = findViewById(R.id.response);
+        submit_btn = findViewById(R.id.submit_btn);
+        new_btn = findViewById(R.id.new_btn);
+        new_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                random_nbr = random.nextInt(100);
+                response_txt.setText("");
+                count = 0;
+                submit_btn.setEnabled(true);
+            }
+        });
+        submit_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(count >= 4) {
+                    Toast.makeText(getApplicationContext(), "You Lose",
+                            Toast.LENGTH_LONG).show();
+                    nbr_txt.setText("");
+                    response_txt.setText("the random number was "+random_nbr);
+                    submit_btn.setEnabled(false);
+                    return;
+                }
+                int user_number = Integer.parseInt(nbr_txt.getText().toString());
+                if(user_number == random_nbr)
+                    response_txt.setText("Excellent !!!");
+                else if (user_number > random_nbr) {
+                    response_txt.setText("Smaller");
+                }else {
+                    response_txt.setText("Bigger");
+                }
+                count++;
+                nbr_txt.setText("");
+                nbr_txt.setFocusable(true);
+            }
+        });
+
     }
 }

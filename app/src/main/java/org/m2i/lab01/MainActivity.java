ae8fd2377ed5936c7c069b9aa0a220e44f0c6512
Fayçal Chebihi <chebihifaycal.it@gmail.com>
2023-05-05 15:22:47 +0100

    add code for settings

--- app/src/main/java/org/m2i/lab01/MainActivity.java
+++ app/src/main/java/org/m2i/lab01/MainActivity.java
@@ -2,6 +2,7 @@ package org.m2i.lab01;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -13,19 +14,25 @@ import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
 
-    static final int COUNT_LIMIT = 5;
+    static int COUNT_LIMIT, MAX_RANG, MIN_RANG;
     Random random = new Random();
     int random_nbr, count=0; // 5 ?
     EditText nbr_txt;
-    TextView response_txt, count_txt;
+    TextView response_txt, count_txt, message_info_txt;
     Button submit_btn, new_btn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Intent intent = getIntent();
+        COUNT_LIMIT = intent.getIntExtra("attempt_max", 5);
+        MAX_RANG = intent.getIntExtra("rang_max", 100);
+        MIN_RANG = intent.getIntExtra("rang_min", 0);
+
         // generate random number
-        random_nbr = random.nextInt(100);
+        random_nbr = random.nextInt(MAX_RANG - MIN_RANG) + MIN_RANG;
     }
 
     @Override
@@ -36,11 +43,15 @@ public class MainActivity extends AppCompatActivity {
         submit_btn = findViewById(R.id.submit_btn);
         new_btn = findViewById(R.id.new_btn);
         count_txt = findViewById(R.id.count_txt);
+        message_info_txt = findViewById(R.id.message_info_txt);
+
         count_txt.setText(String.valueOf(COUNT_LIMIT - count));
+        message_info_txt.setText("the random number is generated between "+MAX_RANG+" and "+MIN_RANG);
+
         new_btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                random_nbr = random.nextInt(100);
+                random_nbr = random.nextInt(MAX_RANG - MIN_RANG) + MIN_RANG;
                 response_txt.setText("");
                 submit_btn.setEnabled(true);
                 count = 0 ;
@@ -50,7 +61,7 @@ public class MainActivity extends AppCompatActivity {
         submit_btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if(count >= 4) {
+                if(count >= COUNT_LIMIT-1) {
                     Toast.makeText(getApplicationContext(), "You Lose",
                             Toast.LENGTH_LONG).show();
                     nbr_txt.setText("");
--- app/src/main/java/org/m2i/lab01/Settings.java
+++ app/src/main/java/org/m2i/lab01/Settings.java
@@ -2,13 +2,45 @@ package org.m2i.lab01;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class Settings extends AppCompatActivity {
+
+    EditText rang_min_txt, rang_max_txt, attempt_max_txt;
+    Button validate_btn ;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        rang_max_txt = findViewById(R.id.max_range_txt);
+        rang_min_txt = findViewById(R.id.min_range_txt);
+        attempt_max_txt = findViewById(R.id.attempt_txt);
+        validate_btn = findViewById(R.id.btn_valid);
+
+        validate_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int min_rang = Integer.parseInt(rang_min_txt.getText().toString());
+                int max_rang = Integer.parseInt(rang_max_txt.getText().toString());
+                int attempt_max = Integer.parseInt(attempt_max_txt.getText().toString());
+
+                Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                intent.putExtra("rang_min",min_rang);
+                intent.putExtra("rang_max",max_rang);
+                intent.putExtra("attempt_max",attempt_max);
+                startActivity(intent);
+            }
+        });
+
     }
 }

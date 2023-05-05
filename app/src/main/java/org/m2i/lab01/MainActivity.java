d557096032abdcdd854de32adb540d0f50248806
Fayçal Chebihi <chebihifaycal.it@gmail.com>
2023-05-05 15:53:21 +0100

    final version

--- app/src/main/java/org/m2i/lab01/MainActivity.java
+++ app/src/main/java/org/m2i/lab01/MainActivity.java
@@ -16,9 +16,9 @@ public class MainActivity extends AppCompatActivity {
 
     static int COUNT_LIMIT, MAX_RANG, MIN_RANG;
     Random random = new Random();
-    int random_nbr, count=0; // 5 ?
+    int random_nbr, count=0, score = 0; // 5 ?
     EditText nbr_txt;
-    TextView response_txt, count_txt, message_info_txt;
+    TextView response_txt, count_txt, message_info_txt, score_txt;
     Button submit_btn, new_btn;
 
     @Override
@@ -44,6 +44,7 @@ public class MainActivity extends AppCompatActivity {
         new_btn = findViewById(R.id.new_btn);
         count_txt = findViewById(R.id.count_txt);
         message_info_txt = findViewById(R.id.message_info_txt);
+        score_txt = findViewById(R.id.score_txt);
 
         count_txt.setText(String.valueOf(COUNT_LIMIT - count));
         message_info_txt.setText("the random number is generated between "+MAX_RANG+" and "+MIN_RANG);
@@ -54,6 +55,7 @@ public class MainActivity extends AppCompatActivity {
                 random_nbr = random.nextInt(MAX_RANG - MIN_RANG) + MIN_RANG;
                 response_txt.setText("");
                 submit_btn.setEnabled(true);
+                nbr_txt.setEnabled(true);
                 count = 0 ;
                 count_txt.setText(String.valueOf(COUNT_LIMIT ));
             }
@@ -61,19 +63,27 @@ public class MainActivity extends AppCompatActivity {
         submit_btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if(count >= COUNT_LIMIT-1) {
+                if(count >= COUNT_LIMIT) {
                     Toast.makeText(getApplicationContext(), "You Lose",
                             Toast.LENGTH_LONG).show();
                     nbr_txt.setText("");
                     response_txt.setText("the random number was "+random_nbr);
                     submit_btn.setEnabled(false);
+                    nbr_txt.setEnabled(false);
                     count = 0 ;
                     count_txt.setText(String.valueOf(COUNT_LIMIT ));
+                    score--;
+                    score_txt.setText("Score = "+score);
                     return;
                 }
                 int user_number = Integer.parseInt(nbr_txt.getText().toString());
-                if(user_number == random_nbr)
+                if(user_number == random_nbr) {
                     response_txt.setText("Excellent !!!");
+                    nbr_txt.setEnabled(false);
+                    submit_btn.setEnabled(false);
+                    score++;
+                    score_txt.setText("Score = "+score);
+                }
                 else if (user_number > random_nbr) {
                     response_txt.setText("Smaller");
                 }else {
